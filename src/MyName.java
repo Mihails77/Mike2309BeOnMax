0bea3cfdd88abd139baf20d287ae9a66ade6f848
mihailsnikolajevs <mike2309@inbox.lv>
2022-12-23 16:20:10 +0200

    InputName

--- src/MyName.java
+++ src/MyName.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class MyName {
     public static void main(String[] args) {
         System.out.println("Please Enter your name:");
-        System.out.println("Your name is :");
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+        System.out.println("Hello , Your name is V" + name);
     }
 
 }
